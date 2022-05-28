a74803bc72e3f1ad23e99d6cbe5c44b5cfd39af7
emad-almajdalawi <majdalawi97@yahoo.com>
2022-05-28 18:18:16 +0300

    fix: pg, add switch user

--- snacks_project/settings.py
+++ snacks_project/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'snacks_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        "USER": 'postgres',
+        "PASSWORD": 'postgres',
+        "HOST": "db",
+        "PORT":5432
     }
 }
 
