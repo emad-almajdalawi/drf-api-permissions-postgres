34eebc4218ccd391e7847cd490b9339a2edbedd0
emad-almajdalawi <majdalawi97@yahoo.com>
2022-05-30 15:52:22 +0300

    very strict permissions

--- snacks_api/api/permissions.py
+++ snacks_api/api/permissions.py
@@ -1,14 +1,29 @@
 from rest_framework import permissions
 
 class OwnerOrReadOnly(permissions.BasePermission):
+    '''Read for anyone. Requires user to be loged-in and the owner of the object to update it'''
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user  # the (owner) is from the model, name of the ForeignKey for get_user_model()
 
+
 class CreateOrReadOnly(permissions.BasePermission):
+    '''Read for anyone. Requires user to be loged-in create objects'''
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
+        return request.user.is_authenticated
+
+
+class OwnerLogin(permissions.BasePermission):
+    '''Very strict authentication, requires user to be loged-in and the owner of the object to read and update it'''
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+class CreateLogin(permissions.BasePermission):
+    '''Very strict authentication, requires user to be loged-in read and create objects'''
+    def has_permission(self, request, view):
         return request.user.is_authenticated
 
